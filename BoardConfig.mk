1b57049f806ea7b122fe004a677b80fc74e87863
mrimp <mrimp@day-off.net>
2017-11-17 10:38:19 -0500

    source

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+# Kernel
+TARGET_KERNEL_CONFIG := greatqlteue_chn_open_defconfig
+
+# Recovery
+TARGET_OTA_ASSERT_DEVICE := greatqlteue
+
+# Inherit common board flags
+include device/samsung/msm8895-common/BoardConfigCommon.mk
---
+++ lineage.mk
@@ -0,0 +1,20 @@
+# Inherit device configuration
+$(call inherit-product, device/samsung/greatqlteue/device_greatqlteue.mk)
+$(call inherit-product, device/samsung/msm8895-common/msm8895-common.mk)
+
+# Inherit from the 64 bit configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_greatqlteue
+PRODUCT_DEVICE := greatqlteue
+PRODUCT_MODEL := SM-N950U1
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
